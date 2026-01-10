e3eb48c7c2fa9178b12c5559163ee1378fe7dcae
chamithsandeepa <chamithsandeepa321@gmail.com>
2026-01-10 12:41:42 +0530

    Refactor Navbar component for improved styling, layout, and user experience

--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { assets } from "../assets/admin_assets/assets";
-import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ setToken }) => {
   return (
-    <div className="flex items-center justify-between py-2 px-[4%]">
-      <img className="w-[max(10%,80px)]" src={assets.logo} alt="Logo" />
+    <div className="sticky top-0 z-50 flex items-center justify-between py-4 px-[4%] glass shadow-soft bg-white/80">
+      <img className="w-[max(10%,120px)]" src={assets.logo} alt="Logo" />
       <button
-        className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm"
+        className="bg-primary text-white border border-primary px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm font-outfit tracking-wide hover:bg-transparent hover:text-primary transition-all duration-300"
         onClick={() => setToken("")}
       >
-        Logout
+        Log out
       </button>
     </div>
   );
